be26e5f8947d35812dd427e9b66d0b6ca258fe68
melanu1991 <melanu284733@gmail.com>
2017-09-05 16:45:04 +0300

    имплементация базового набора функций по работе с БД

--- RSS-reader/RSS-reader/VAKDataManager.h
+++ RSS-reader/RSS-reader/VAKDataManager.h
@@ -1,5 +1,15 @@
 #import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
 
 @interface VAKDataManager : NSObject
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
++ (VAKDataManager *)sharedManager;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
 
 @end
