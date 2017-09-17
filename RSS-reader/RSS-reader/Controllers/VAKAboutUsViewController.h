e1d16ca3d842be5c29628d7c263e2169f279f95e
melanu1991 <melanu284733@gmail.com>
2017-09-17 20:59:09 +0300

    Создание экрана About us with VAKAboutUsViewController

---
+++ RSS-reader/RSS-reader/Controllers/VAKAboutUsViewController.h
@@ -0,0 +1,5 @@
+#import <UIKit/UIKit.h>
+
+@interface VAKAboutUsViewController : UIViewController
+
+@end
---
+++ RSS-reader/RSS-reader/Controllers/VAKAboutUsViewController.m
@@ -0,0 +1,13 @@
+#import "VAKAboutUsViewController.h"
+
+@interface VAKAboutUsViewController ()
+
+@end
+
+@implementation VAKAboutUsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
