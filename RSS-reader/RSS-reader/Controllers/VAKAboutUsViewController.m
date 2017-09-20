453d7662a9b2ea5079090fd9c8bef02c9ddd9c8f
melanu1991 <melanu284733@gmail.com>
2017-09-20 23:04:24 +0300

    Начало работы над VAKAboutUsViewController

--- RSS-reader/RSS-reader/Controllers/VAKAboutUsViewController.m
+++ RSS-reader/RSS-reader/Controllers/VAKAboutUsViewController.m
@@ -1,13 +1,55 @@
 #import "VAKAboutUsViewController.h"
+#import "VAKSlideMenuViewController.h"
 
 @interface VAKAboutUsViewController ()
+
+@property (strong, nonatomic) VAKSlideMenuViewController *slideMenuVC;
 
 @end
 
 @implementation VAKAboutUsViewController
 
+#pragma mark - lazy getters
+
+- (VAKSlideMenuViewController *)slideMenuVC {
+    if (!_slideMenuVC) {
+        _slideMenuVC = [[VAKSlideMenuViewController alloc] init];
+    }
+    return _slideMenuVC;
+}
+
+#pragma mark - lifecycle view controller
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+}
+
+#pragma mark - Actions
+
+- (IBAction)slideMenuButtonPressed:(UIBarButtonItem *)sender {
+    
+}
+
+- (IBAction)menuAboutUsButtonPressed:(UIButton *)sender {
+    switch (sender.tag) {
+        case 0:
+        {
+            
+        }
+            break;
+        case 1:
+        {
+            
+        }
+            break;
+        case 2:
+        {
+            
+        }
+            break;
+        default:
+            break;
+    }
 }
 
 @end
