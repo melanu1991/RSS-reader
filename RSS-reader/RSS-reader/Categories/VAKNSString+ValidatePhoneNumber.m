f40924bba6515b8cbcbe51959401eed4ac4cb65f
melanu1991 <melanu284733@gmail.com>
2017-09-23 00:41:18 +0300

    finished method validate phone number

--- RSS-reader/RSS-reader/Categories/VAKNSString+ValidatePhoneNumber.m
+++ RSS-reader/RSS-reader/Categories/VAKNSString+ValidatePhoneNumber.m
@@ -3,7 +3,15 @@
 @implementation NSString (ValidatePhoneNumber)
 
 - (BOOL)isValidPhoneNumber {
-    return YES;
+    NSString *pattern = @"^[+][3][7][5][(](([4][4])|([2][5])|([2][9])|([3][3]))[)][1-9]\\d{2}[-]\\d{2}[-]\\d{2}";
+    NSError *error;
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:pattern options:NSRegularExpressionCaseInsensitive error:&error];
+    NSLog(@"%@", error);
+    NSUInteger number = [regex numberOfMatchesInString:self options:0 range:NSMakeRange(0, self.length)];
+    if (number > 0) {
+        return YES;
+    }
+    return NO;
 }
 
 @end
